def54f9302d9d540f7613a932c57156871ae2c68
DominicEdwardLee <dominic@insiris.com>
2021-09-16 23:18:12 +0100

    finished generating random shapes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,18 +4,21 @@ import {Campaign} from './domain/campaign.model';
 import {Gang} from 'src/app/domain/gang.model'
 import {Territory} from './domain/territory.model';
 import {NgxSvgModule} from 'ngx-svg';
+import {parse} from "@angular/compiler/src/render3/view/style_parser";
 
-interface Polygon {
+interface Hexagon {
   points: any[];
   centre: number[];
   borderSize: number;
   borderColor?: string;
   fill?: string;
+  mouseOver?: string;
   clicked?: boolean;
   territory?: Territory;
+  location?: number[];
 }
 
-const a = 50;
+const a = 30;
 
 // width
 // var w = a * 2;
@@ -35,7 +38,7 @@ export class AppComponent {
   // title = 'Necromunda Campaign Tracker';
 
   hexagon2DArray: any[] = [];
-  hexagonList: Polygon[] = [];
+  hexagonList: Hexagon[] = [];
   territories = [
     {id: 1},
     {id: 2},
@@ -48,10 +51,48 @@ export class AppComponent {
     {id: 9},
     {id: 10},
     {id: 11},
+    {id: 3},
+    {id: 4},
+    {id: 5},
+    {id: 6},
+    {id: 7},
+    {id: 8},
+    {id: 9},
+    {id: 10},
+    {id: 11},
+    {id: 3},
+    {id: 4},
+    {id: 5},
+    {id: 6},
+    {id: 7},
+    {id: 6},
+    {id: 7},
+    {id: 8},
+    {id: 9},
+    {id: 10},
+    {id: 11},
+    {id: 3},
+    {id: 4},
+    {id: 5},
+    {id: 6},
+    {id: 7},
+    {id: 8},
+    {id: 9},
+    {id: 10},
+    {id: 11},
+    {id: 3},
+    {id: 4},
+    {id: 5},
+    {id: 6},
+    {id: 7},
+    {id: 8},
+    {id: 9},
+    {id: 10},
+    {id: 11},
     {id: 12}
   ]
 
-  height = 800;
+  height = 1000;
 
   h: number;
   w: number;
@@ -59,15 +100,33 @@ export class AppComponent {
   constructor() {
 
     this.hexagon2DArray = _buildHexagonList(this.height, this.height);
-    rowLength = this.hexagon2DArray.length;
-    columnLength = this.hexagon2DArray[0].length;
-
 
     this.hexagonList = [].concat(...this.hexagon2DArray);
 
-    _selectHexagonsForTerritories(
-      this.territories,
-      this.hexagon2DArray);
+    var selectedAndOptions = _selectHexagonsForTerritories(this.territories);
+
+
+    selectedAndOptions[0].map(code => _codeToCoord(code)).forEach(coord =>
+      this.hexagon2DArray[coord[0]][coord[1]].fill = 'rgba(200, 50, 50, 0.2)'
+    );
+
+    this.hexagon2DArray[selectedAndOptions[2][0]][selectedAndOptions[2][1]].fill = 'rgba(250, 250, 250, 0.2)'
+
+    selectedAndOptions[1].map(code => _codeToCoord(code)).forEach(coord => {
+        var hex = this.hexagon2DArray[coord[0]][coord[1]]
+
+        hex.fill = hex.fill.replace(/, 0.2\)/i, ', 0.5)');
+      }
+    );
+
+  }
+
+  coordToInt(numArr) {
+    return [parseInt(numArr[0].toString()), parseInt(numArr[1].toString())];
+  }
+
+  locationToCode(location):string {
+    return (location[0] + location[1]*_multiplier()).toString()
   }
 
   mouseOver(hexagon) {
@@ -78,7 +137,7 @@ export class AppComponent {
 
   mouseOut(hexagon) {
     if (!hexagon.clicked) {
-      hexagon.fill = 'rgba(100, 100, 20, 0.2)'
+      hexagon.fill = 'rgba(100, 100,s 20, 0.2)'
     }
   }
 
@@ -93,57 +152,52 @@ export class AppComponent {
 }
 
 
+function _selectHexagonsForTerritories(territories) {
+  let seedCoord: number[] = [getRandomArbitrary(1, rowLength - 2), getRandomArbitrary(1, columnLength - 2)];
+  var selectedHexagonCodes: number[] = [_coordToCode(seedCoord)];
+  var selectableOptions: number[] = _getNewOptions(_coordToCode(seedCoord), selectedHexagonCodes);
 
-function _selectHexagonsForTerritories(territories, hexagon2DArray) {
-  let seedIndex = [getRandomArbitrary(1, rowLength - 2), getRandomArbitrary(1, columnLength - 2)];
+  while (selectedHexagonCodes.length < territories.length &&
+  selectedHexagonCodes.length < (columnLength * rowLength) - ((columnLength - 1) * 2) + ((rowLength - 1) * 2)) {
 
-  var selectedHexagonIndexes = [];
-  var selectableOptions = [];
-  _addNewOptions(seedIndex, selectedHexagonIndexes, selectableOptions);
+    var hexagonIndexCode: number = selectableOptions[getRandomArbitrary(1, selectableOptions.length - 1)];
 
-  while (selectedHexagonIndexes.length < territories.length &&
-  selectedHexagonIndexes.length < (columnLength * rowLength) - ((columnLength - 1) * 2) + ((rowLength - 1) * 2)) {
+    if (!selectedHexagonCodes.includes(hexagonIndexCode)) {
 
-    var hexagonIndexCode = selectableOptions[getRandomArbitrary(1, selectableOptions.length - 1)];
-    var hexagonIndex = _codeToCoord(hexagonIndexCode);
+      selectedHexagonCodes.push(hexagonIndexCode);
 
-    if (!selectedHexagonIndexes.includes(hexagonIndexCode)) {
+      selectableOptions = selectableOptions.filter(code => code !== hexagonIndexCode);
 
-      selectedHexagonIndexes.push(hexagonIndexCode);
-
-      _removeOption(hexagonIndexCode, selectableOptions);
-
-      _addNewOptions(hexagonIndexCode, selectedHexagonIndexes, selectableOptions);
+      selectableOptions.push(..._getNewOptions(hexagonIndexCode, selectedHexagonCodes));
 
     }
   }
+
+  return [selectedHexagonCodes, selectableOptions, seedCoord];
 }
 
-function _codeToCoord(hexagonIndexCode: number) {
-  return []
-}
-function _coordToCcode(hexagonCoord: number[]) {
-  return 0
-}
+function _getNewOptions(hexagonIndexCode, selectedHexagonCodes): number[] {
+  var allowedCoords = _generateSurroundingHexagonCodes(_codeToCoord(hexagonIndexCode)).filter(coord =>
+    coord[0] > 0 && coord[0] < rowLength - 1 &&
+    coord[1] > 0 && coord[1] < columnLength - 1
+  )
 
-function _removeOption(hexagonIndexCode, selectableOptions) {
-  var optionIndex = selectableOptions.findIndex(hexagonIndex);
-  if (optionIndex) {
-    selectableOptions.splice(optionIndex, 1);
-  }
+  var codes = allowedCoords.map(coord => _coordToCode(coord));
+
+  return codes.filter(code => !selectedHexagonCodes.includes(code));
 }
 
-function _addNewOptions(hexagonIndexCode, selectedHexagonIndexes, selectableOptions) {
+function _generateSurroundingHexagonCodes(coord: number[]) {
+  const evenOddAdjust = (coord[0] % 2 == 0) ? -1 : 1;
 
-  _generateSeroundingHexagonCodes(_codeToCoord(hexagonIndexCode);
-//todo
-  selectableOptions.push();
-}
-
-function _generateSeroundingHexagonCodes(coord: number[]) {
-  var surroundingIndexCodes = [];
-  //todo
-  return surroundingIndexCodes
+  return [
+    [coord[0], coord[1] + 1],
+    [coord[0], coord[1] - 1],
+    [coord[0] - 1, coord[1]],
+    [coord[0] + 1, coord[1]],
+    [coord[0] - 1, coord[1] + evenOddAdjust],
+    [coord[0] + 1, coord[1] + evenOddAdjust],
+  ]
 }
 
 function _buildHexagonList(width, height) {
@@ -153,12 +207,16 @@ function _buildHexagonList(width, height) {
   for (let x = a * 2; x <= (width - a * 2); x += a * 1.5) {
     var hexagonRow = []
     for (let y = a * 2; y <= (height - a * 2); y += h) {
-      hexagonRow.push(_buildHexagon(x, alternate ? y + h / 2 : y));
+      var hex: Hexagon = _buildHexagon(x, alternate ? y + h / 2 : y)
+      hex.location = [hexagonRow.length, hexagonList.length]
+      hexagonRow.push(hex);
     }
     alternate = !alternate
     hexagonList.push(hexagonRow);
   }
 
+  rowLength = hexagonList.length;
+  columnLength = hexagonList[0].length;
   return hexagonList;
 }
 
@@ -180,6 +238,19 @@ function _calcHexagonCoOrds(x: number, y: number) {
   ]);
 }
 
+
 function getRandomArbitrary(min, max) {
   return Math.round(Math.random() * (max - min) + min);
+}
+
+function _codeToCoord(hexagonIndexCode: number) {
+  return [hexagonIndexCode % _multiplier(), parseInt((hexagonIndexCode / _multiplier()).toString())]
+}
+
+function _coordToCode(hexagonCoord: number[]) {
+  return hexagonCoord[0] + hexagonCoord[1] * _multiplier();
+}
+
+function _multiplier() {
+  return rowLength > columnLength ? rowLength : columnLength;
 }
