d7ac5689630d1ec8423fed33316210c6e02f6eed
Dominic Lee <dominicedwardlee@gmail.com>
2021-09-17 14:04:57 +0100

    fix edge bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import {CampaignService} from './data/campaign.service';
 import {Campaign} from './domain/campaign.model';
 import {Gang} from 'src/app/domain/gang.model'
 import {Territory} from './domain/territory.model';
-import {NgxSvgModule} from 'ngx-svg';
-import {parse} from "@angular/compiler/src/render3/view/style_parser";
 
 interface Hexagon {
   points: any[];
@@ -12,13 +10,15 @@ interface Hexagon {
   borderSize: number;
   borderColor?: string;
   fill?: string;
-  mouseOver?: string;
+  clickedColour?: string;
+  mouseOverColour?: string;
+  mouseOutColour?: string;
   clicked?: boolean;
   territory?: Territory;
   location?: number[];
 }
 
-const a = 30;
+const a = 70;
 
 // width
 // var w = a * 2;
@@ -51,45 +51,7 @@ export class AppComponent {
     {id: 9},
     {id: 10},
     {id: 11},
-    {id: 3},
-    {id: 4},
-    {id: 5},
-    {id: 6},
-    {id: 7},
-    {id: 8},
-    {id: 9},
-    {id: 10},
-    {id: 11},
-    {id: 3},
-    {id: 4},
-    {id: 5},
-    {id: 6},
-    {id: 7},
-    {id: 6},
-    {id: 7},
-    {id: 8},
-    {id: 9},
-    {id: 10},
-    {id: 11},
-    {id: 3},
-    {id: 4},
-    {id: 5},
-    {id: 6},
-    {id: 7},
-    {id: 8},
-    {id: 9},
-    {id: 10},
-    {id: 11},
-    {id: 3},
-    {id: 4},
-    {id: 5},
-    {id: 6},
-    {id: 7},
-    {id: 8},
-    {id: 9},
-    {id: 10},
-    {id: 11},
-    {id: 12}
+    {id: 12},
   ]
 
   height = 1000;
@@ -98,52 +60,77 @@ export class AppComponent {
   w: number;
 
   constructor() {
-
     this.hexagon2DArray = _buildHexagonList(this.height, this.height);
 
     this.hexagonList = [].concat(...this.hexagon2DArray);
-
   }
 
   coordToInt(numArr) {
     return [parseInt(numArr[0].toString()), parseInt(numArr[1].toString())];
   }
 
-  locationToCode(location): string {
+  locationToCoord(location): string {
     return (location[0] + location[1] * _multiplier()).toString()
   }
 
   mouseOver(hexagon) {
-    if (!hexagon.clicked) {
-      hexagon.fill = 'rgba(100, 100, 20, 0.5)'
+    if (!hexagon.clicked && hexagon.mouseOverColour) {
+      hexagon.fill = hexagon.mouseOverColour
     }
   }
 
   mouseOut(hexagon) {
-    if (!hexagon.clicked) {
-      hexagon.fill = 'rgba(100, 100,s 20, 0.2)'
+    if (!hexagon.clicked && hexagon.mouseOutColour) {
+      hexagon.fill = hexagon.mouseOutColour
     }
   }
 
   onClick(hexagon) {
-    var selectedAndOptions = _selectHexagonsForTerritories(hexagon.location, this.territories);
+    var seedCoord = hexagon.location
+    if (   seedCoord[0] > 0 && seedCoord[0] < rowLength - 1 &&
+      seedCoord[1] > 0 && seedCoord[1] < columnLength - 1) {
 
+      this.hexagonList.forEach(hex => hex.fill = 'rgba(100, 100, 20, 0.2)')
 
-    selectedAndOptions[0].map(code => _codeToCoord(code)).forEach(coord =>
-      this.hexagon2DArray[coord[0]][coord[1]].fill = 'rgba(200, 50, 50, 0.2)'
-    );
+      var selectedHexagonCodes = _selectHexagonsForTerritories(hexagon.location, this.territories);
 
-    this.hexagon2DArray[selectedAndOptions[2][0]][selectedAndOptions[2][1]].fill = 'rgba(250, 250, 250, 0.2)'
-
-    selectedAndOptions[1].map(code => _codeToCoord(code)).forEach(coord => {
-        var hex = this.hexagon2DArray[coord[0]][coord[1]]
-
-        hex.fill = hex.fill.replace(/, 0.2\)/i, ', 0.5)');
-      }
-    );
+      _setSelectedColours(selectedHexagonCodes, this.hexagon2DArray);
+    }
   }
 }
 
+function _setSelectedColours(codeList, hexagon2DArray) {
+  // codeList.map(code => _codeToCoord(code)).forEach(coord => {
+  //   var hex = hexagon2DArray[coord[0]][coord[1]];
+  //   hex.clickedColour = 'rgba(200, 50, 50, 0.8)';
+  //   hex.mouseOverColour = 'rgba(200, 50, 50, 0.5)';
+  //   hex.mouseOutColour = 'rgba(200, 50, 50, 0.2)';
+  //
+  //   hex.fill = hex.mouseOutColour;
+  // });
+
+  hexagon2DArray.forEach((row, x) => {
+    row.forEach((hex, y) => {
+
+      if (codeList.includes(_coordToCode([x, y]))) {
+        //  selected
+        hex.clickedColour = 'rgba(200, 50, 50, 0.8)';
+        hex.mouseOverColour = 'rgba(200, 50, 50, 0.5)';
+        hex.mouseOutColour = 'rgba(200, 50, 50, 0.2)';
+
+        hex.fill = hex.mouseOutColour;
+      } else {
+        //  border
+        hex.clickedColour = 'rgba(100, 100, 50, 0.8)';
+        hex.mouseOverColour = 'rgba(100, 100, 50, 0.5)';
+        hex.mouseOutColour = 'rgba(100, 100, 50, 0.2)';
+
+        hex.fill = hex.mouseOutColour;
+      }
+
+    });
+  });
+}
 
 function _selectHexagonsForTerritories(seedCoord, territories) {
   var selectedHexagonCodes: number[] = [_coordToCode(seedCoord)];
@@ -160,12 +147,15 @@ function _selectHexagonsForTerritories(seedCoord, territories) {
 
       selectableOptions = selectableOptions.filter(code => code !== hexagonIndexCode);
 
-      selectableOptions.push(..._getNewOptions(hexagonIndexCode, selectedHexagonCodes));
+      selectableOptions.push(...[].concat(...selectedHexagonCodes.map(code => _getNewOptions(code, selectedHexagonCodes))));
+
+      // more spikey...
+      // selectableOptions.push(..._getNewOptions(hexagonIndexCode, selectedHexagonCodes));
 
     }
   }
 
-  return [selectedHexagonCodes, selectableOptions, seedCoord];
+  return selectedHexagonCodes;
 }
 
 function _getNewOptions(hexagonIndexCode, selectedHexagonCodes): number[] {
@@ -218,6 +208,9 @@ function _buildHexagon(x, y) {
     centre: [x, y],
     borderSize: 2,
     borderColor: 'rgba(125, 125, 32, 0.8)',
+    clickedColour: 'rgba(100, 100, 20, 0.8)',
+    mouseOverColour: 'rgba(100, 100, 20, 0.5)',
+    mouseOutColour: 'rgba(100, 100, 20, 0.2)',
     fill: 'rgba(100, 100, 20, 0.2)'
   }
 }
