787bcd5201d76ff1519cc0112d90e3b5f4efa491
DominicEdwardLee <dominic@insiris.com>
2021-09-28 08:38:47 +0100

    added it to app component ready to be put wherever now

--- src/app/presentation/campaignwizard/generate-map/generate-map.component.ts
+++ src/app/presentation/campaignwizard/generate-map/generate-map.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Territory} from '../../../domain/territory.model';
 import {ProgressBarMode} from '@angular/material/progress-bar';
 import {Hexagon} from "../../../domain/hexagon.model";
@@ -16,7 +16,7 @@ enum Mode {
   templateUrl: './generate-map.component.html',
   styleUrls: ['./generate-map.component.css']
 })
-export class GenerateMapComponent {
+export class GenerateMapComponent implements OnInit {
 
   hexagon2DArray: Hexagon[][];
   hexagonList: Hexagon[];
@@ -27,29 +27,18 @@ export class GenerateMapComponent {
   progressMode: ProgressBarMode = "buffer"
   assignableTerritories: Territory[] = [];
 
-  territories: Territory[] = [
-    {id: 1, name: "TERRITORY NAME 1", gangID: 2},
-    {id: 2, name: "TERRITORY NAME 2"},
-    {id: 3, name: "TERRITORY NAME 3"},
-    {id: 4, name: "TERRITORY NAME 4"},
-    {id: 5, name: "TERRITORY NAME 5"},
-    {id: 6, name: "TERRITORY NAME 6"},
-    {id: 7, name: "TERRITORY NAME 7"},
-    {id: 8, name: "TERRITORY NAME 8"},
-    {id: 8, name: "TERRITORY NAME 9"},
-    {id: 10, name: "TERRITORY NAME 10", gangID: 3},
-    {id: 8, name: "TERRITORY NAME 8", gangID: 1},
-    {id: 8, name: "TERRITORY NAME 9"},
-    {id: 10, name: "TERRITORY NAME 10"},
-    {id: 11, name: "TERRITORY NAME 11"},
-    {id: 12, name: "TERRITORY NAME 12"},
-  ]
+  @Input()
+  territories: Territory[];
 
   height = 2000;
   viewBox = [0, 0, this.height, this.height];
 
   constructor() {
-    var sideLength = (80 - ((this.territories.length > 9 ? this.territories.length - 9 : 0) * 2)) * (this.height/1000)
+
+  }
+
+  ngOnInit(): void {
+    var sideLength = (80 - ((this.territories.length > 9 ? this.territories.length - 9 : 0) * 2)) * (this.height / 1000)
 
     if (this.hexagon2DArray) {
       this.mode = Mode.Read
@@ -60,6 +49,7 @@ export class GenerateMapComponent {
 
     this.hexagonList = [].concat(...this.hexagon2DArray);
   }
+
 
   onClick(hexagon) {
     if (hexagon.border)
@@ -94,7 +84,7 @@ export class GenerateMapComponent {
     }
   }
 
-  percentageSelected() : number {
+  percentageSelected(): number {
     if (!this.editMode()) {
       return 0;
     }
@@ -123,7 +113,7 @@ export class GenerateMapComponent {
     this.progressMode = "determinate";
   }
 
-  removeTerritory(hexagon:Hexagon) {
+  removeTerritory(hexagon: Hexagon) {
     this.assignableTerritories.unshift(hexagon.territory);
     hexagon.setTerritory(null);
   }
@@ -165,11 +155,11 @@ export class GenerateMapComponent {
     this.percentageSelected();
   }
 
-  rowLength():number {
+  rowLength(): number {
     return this.hexagon2DArray.length;
   }
 
-  columnLength():number {
+  columnLength(): number {
     return this.hexagon2DArray[0].length;
   }
 
--- src/app/presentation/home/home/home.component.ts
+++ src/app/presentation/home/home/home.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { CampaignService } from 'src/app/data/campaign.service';
-import { MatDialog } from '@angular/material/dialog';
-import { CampaignSelectorComponent } from '../campaign-selector/campaign-selector.component';
-import { NewCampaignComponent } from '../new-campaign/new-campaign.component';
+import {Component, OnInit} from '@angular/core';
+import {CampaignService} from 'src/app/data/campaign.service';
+import {MatDialog} from '@angular/material/dialog';
+import {CampaignSelectorComponent} from '../campaign-selector/campaign-selector.component';
+import {NewCampaignComponent} from '../new-campaign/new-campaign.component';
+import {Territory} from "../../../domain/territory.model";
 
 @Component({
   selector: 'app-home',
@@ -12,12 +13,30 @@ import { NewCampaignComponent } from '../new-campaign/new-campaign.component';
 export class HomeComponent implements OnInit {
   campaigns: any;
 
+  territories: Territory[] = [
+    {id: 1, name: "TERRITORY NAME 1", gangID: 2},
+    {id: 2, name: "TERRITORY NAME 2"},
+    {id: 3, name: "TERRITORY NAME 3"},
+    {id: 4, name: "TERRITORY NAME 4"},
+    {id: 5, name: "TERRITORY NAME 5"},
+    {id: 6, name: "TERRITORY NAME 6"},
+    {id: 7, name: "TERRITORY NAME 7"},
+    {id: 8, name: "TERRITORY NAME 8"},
+    {id: 8, name: "TERRITORY NAME 9"},
+    {id: 10, name: "TERRITORY NAME 10", gangID: 3},
+    {id: 8, name: "TERRITORY NAME 8", gangID: 1},
+    {id: 8, name: "TERRITORY NAME 9"},
+    {id: 10, name: "TERRITORY NAME 10"},
+    {id: 11, name: "TERRITORY NAME 11"},
+    {id: 12, name: "TERRITORY NAME 12"},
+  ]
+
 
   constructor(
     private campaignService: CampaignService,
     public dialog: MatDialog
-
-  ) { }
+  ) {
+  }
 
 
   // openDialog() {
@@ -32,7 +51,7 @@ export class HomeComponent implements OnInit {
     let dialogref = this.dialog.open(CampaignSelectorComponent,
       {
         width: '500px',
-       // height: '500px'
+        // height: '500px'
       }
     );
   }
@@ -55,11 +74,11 @@ export class HomeComponent implements OnInit {
   // }
 
 
-
 }
 
 @Component({
   selector: 'dialog-content-example-dialog',
   templateUrl: 'dialog-content-example-dialog.html',
 })
-export class DialogContentExampleDialog { }
+export class DialogContentExampleDialog {
+}
